ccd6af423fafb941d5391cfa1b86162a6056d710
NeelambaranPP <145982256+NeelambaranPP@users.noreply.github.com>
2025-10-15 09:24:35 +0530

    student.py

--- cal.py
+++ cal.py
@@ -1,7 +1,14 @@
-print("Hello World")
-print("neela")
-a=9
-b=34
-print(a+b)
-name = input("What is your name? ")
-print(f"Hello, {name}! 👋")
+students = []
+def add_student(name, regno):
+    student = {"name": name, "regno": regno}
+    students.append(student)
+    return student
+def get_all_students():
+    return students
+if __name__ == "__main__":
+    add_student("Neel", "SSN001")
+    add_student("kapil", "SSN002")
+    print("Student List:")
+
+    for s in get_all_students():
+        print(f"Name: {s['name']} | RegNo: {s['regno']}")
